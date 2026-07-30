1515463f4518c623984a0ea5505655332641d2ef
GeraldDM21 <gerald.delmon@gmail.com>
2026-07-30 10:46:26 -0600

    Add categoria form

--- src/app/pages/categoria-form/categoria-form.ts
+++ src/app/pages/categoria-form/categoria-form.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgIf } from '@angular/common';
+import { CategoriaService } from '../../services/categoria';
+
 
 @Component({
   selector: 'app-categoria-form',
-  imports: [],
+  imports: [FormsModule, NgIf, RouterLink],
   templateUrl: './categoria-form.html',
-  styleUrl: './categoria-form.scss',
+  styleUrl: './categoria-form.scss'
 })
-export class CategoriaForm {}
+export class CategoriaForm implements OnInit {
+  categoria = { nombre: '', descripcion: '' };
+  id: number | null = null;
+  isEdit = false;
+  error = '';
+
+  constructor(
+    private categoriaService: CategoriaService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    if (this.id) {
+      this.isEdit = true;
+      this.categoriaService.getById(this.id).subscribe({
+        next: (res) => { this.categoria = res; },
+        error: () => { this.error = 'Error al cargar categoría'; }
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.isEdit && this.id) {
+      this.categoriaService.update(this.id, this.categoria).subscribe({
+        next: () => this.router.navigate(['/categorias']),
+        error: () => { this.error = 'Error al actualizar'; }
+      });
+    } else {
+      this.categoriaService.create(this.categoria).subscribe({
+        next: () => this.router.navigate(['/categorias']),
+        error: () => { this.error = 'Error al crear'; }
+      });
+    }
+  }
+}
