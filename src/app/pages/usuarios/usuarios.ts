f316a446868b975b3db95f33d35e7d56e14ee32b
GeraldDM21 <gerald.delmon@gmail.com>
2026-07-30 10:55:55 -0600

    Add usuarios

--- src/app/pages/usuarios/usuarios.ts
+++ src/app/pages/usuarios/usuarios.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgIf, NgFor } from '@angular/common';
+import { UsuarioService } from '../../services/usuario';
 
 @Component({
   selector: 'app-usuarios',
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './usuarios.html',
-  styleUrl: './usuarios.scss',
+  styleUrl: './usuarios.scss'
 })
-export class Usuarios {}
+export class Usuarios implements OnInit {
+  usuarios: any[] = [];
+  totalPages = 0;
+  currentPage = 0;
+  error = '';
+
+  constructor(private usuarioService: UsuarioService) {}
+
+  ngOnInit(): void {
+    this.loadUsuarios();
+  }
+
+  loadUsuarios(page: number = 0): void {
+    this.usuarioService.getAll(page).subscribe({
+      next: (res) => {
+        this.usuarios = res.content;
+        this.totalPages = res.totalPages;
+        this.currentPage = res.number;
+      },
+      error: () => { this.error = 'Error al cargar usuarios'; }
+    });
+  }
+}
