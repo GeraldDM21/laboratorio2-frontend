328ea939efe34b47131175fd74ccf701d71ece47
GeraldDM21 <gerald.delmon@gmail.com>
2026-07-30 10:29:13 -0600

    Feat: Add login

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink, NgIf],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
-export class Login {}
+export class Login {
+  email = '';
+  password = '';
+  error = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit(): void {
+    this.authService.login(this.email, this.password).subscribe({
+      next: (res) => {
+        this.authService.saveToken(res.token);
+        this.authService.saveUser(res.user);
+        this.router.navigate(['/categorias']);
+      },
+      error: () => {
+        this.error = 'Credenciales inválidas';
+      }
+    });
+  }
+}
